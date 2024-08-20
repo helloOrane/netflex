3ad6abd8e261e075b7a3a2c77d4c3eb42ce3c5af
Orane Nelson <cenelson.orane@gmail.com>
2024-08-20 14:14:50 +0200

    start design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ module.exports = {
   content: [
     "./templates/**/*.html",
     "./static/src/**/*.js",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
+    colors: {
+      "amber-400": "#fbbf24",
+      "amber-600": "#d97706",
+    },
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
