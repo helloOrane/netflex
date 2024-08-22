ce139613aa9e4695060ab7cea92d1e9a16285f98
Orane Nelson <cenelson.orane@gmail.com>
2024-08-22 12:33:21 +0200

    (design) implement slider on home page

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	return render_template("pages/home/index.html")
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
